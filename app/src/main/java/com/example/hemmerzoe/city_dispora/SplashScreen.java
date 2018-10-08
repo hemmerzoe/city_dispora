9918962869774005379019224732efbe121bb5a2
hemmerzoe <hemmerzoe@gmail.com>
2018-10-08 15:02:31 +0700

    fix

---
+++ app/src/main/java/com/example/hemmerzoe/city_dispora/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.hemmerzoe.city_dispora;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashscreen);
+
+        Thread thread = new Thread(){
+            public void run(){
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    startActivity( new Intent(SplashScreen.this,MainActivity.class));
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+}
