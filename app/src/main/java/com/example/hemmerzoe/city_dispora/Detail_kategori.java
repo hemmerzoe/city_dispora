5a91556531f10accc26dcceab00329c6ced6d957
Anisah Denis <anisahdenis.1595@gmail.com>
2018-10-31 15:56:37 +0700

    update details

--- app/src/main/java/com/example/hemmerzoe/city_dispora/Detail_kategori.java
+++ app/src/main/java/com/example/hemmerzoe/city_dispora/Detail_kategori.java
@@ -1,20 +1,35 @@
 package com.example.hemmerzoe.city_dispora;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.hemmerzoe.city_dispora.utils.Tools;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Detail_kategori extends AppCompatActivity {
+public class Detail_kategori extends AppCompatActivity implements OnMapReadyCallback{
     private ActionBar actionBar;
     private Toolbar toolbar;
-    public MainActivity menu2;
+    public GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,19 @@ public class Detail_kategori extends AppCompatActivity {
         //menu2.initNavigationMenu();
         initToolbar();
         initComponent();
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapPlaces);
+        mapFragment.getMapAsync(this);
+
+         ((ImageView) findViewById(R.id.image_1)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogAbout();
+                }
+                });
+
+
     }
 
     private void initToolbar() {
@@ -57,4 +85,56 @@ public class Detail_kategori extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-7.6285332, 112.8894963);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("karangketug"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,12));
+    }
+
+    private void showDialogAbout() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_image);
+        dialog.setCancelable(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+//        ((TextView) dialog.findViewById(R.id.tv_version)).setText("Version " + BuildConfig.VERSION_NAME);
+//
+//        ((View) dialog.findViewById(R.id.bt_getcode)).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(Intent.ACTION_VIEW);
+//                i.setData(Uri.parse("https://codecanyon.net/user/dream_space/portfolio"));
+//                startActivity(i);
+//            }
+//        });
+
+        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+//        ((Button) dialog.findViewById(R.id.bt_rate)).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Tools.rateAction(AboutDialogMainAction.this);
+//            }
+//        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
+    }
 }
